1da195d08bde150e44903da74b55e771e4b1e91c
yanghojoon <yanghojoon8487@gmail.com>
2022-02-22 22:21:58 +0900

    Initial Commit

---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by 양호준 on 2022/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
